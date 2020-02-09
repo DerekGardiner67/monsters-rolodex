b618a27a7b7edd06bfa7395257293d2310b2bc42
Derek Gardiner <derekgardiner@derekgainersmbp.home>
2020-02-09 15:39:52 +0000

    Updates

---
+++ precache-manifest.4adb8fb802fa24cc353cc1533e3c3abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7a216c2bbe18badecda2ebf8c3b09d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8fafeb176b9dad5dc073",
+    "url": "/monsters-rolodex/static/css/main.3e281a01.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fafeb176b9dad5dc073",
+    "url": "/monsters-rolodex/static/js/main.5ac0acce.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
